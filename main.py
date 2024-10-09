43a98352cca6c2824eddcc51cb1394c837cac72e
Ruslan <damin4@mail.ru>
2024-10-09 21:01:28 +0300

    Test open3d, we are generate pointcloud and show it version open3d:0.17.0

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import open3d as o3d
 import numpy as np
 ply_point_cloud = o3d.data.PLYPointCloud()
-pcd = o3d.io.read_point_cloud(ply_point_cloud.path)
+
+num_points = 1000
+points = np.random.rand(num_points, 3)
+
+pcd = o3d.geometry.PointCloud()
+pcd.points = o3d.utility.Vector3dVector(points)
 print(pcd)
 print(np.asarray(pcd.points))
-o3d.visualization.draw_plotly([pcd],
-                                  zoom=0.3412,
-                                  front=[0.4257, -0.2125, -0.8795],
-                                  lookat=[2.6172, 2.0475, 1.532],
-                                  up=[-0.0694, -0.9768, 0.2024])
+o3d.visualization.draw_plotly([pcd])
